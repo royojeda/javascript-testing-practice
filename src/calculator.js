5f14075b8d9ccff245a1547d956c7ebc7f770766
Dennis Ojeda <royojeda1@gmail.com>
2022-12-19 07:04:47 +0800

    Add Calculator.subtract()

--- src/calculator.js
+++ src/calculator.js
@@ -2,4 +2,8 @@ export default class Calculator {
   static add(x, y) {
     return x + y;
   }
+
+  static subtract(x, y) {
+    return x - y;
+  }
 }
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -13,3 +13,17 @@ describe("add", () => {
     expect(Calculator.add(999.7, -23.4)).toBeCloseTo(976.3);
   });
 });
+
+describe("subtract", () => {
+  test("works with integers", () => {
+    expect(Calculator.subtract(678, 55)).toStrictEqual(623);
+  });
+
+  test("works with another set of integers", () => {
+    expect(Calculator.subtract(-43, -100)).toStrictEqual(57);
+  });
+
+  test("works with floats", () => {
+    expect(Calculator.subtract(1.1, 77.32)).toBeCloseTo(-76.22);
+  });
+});
